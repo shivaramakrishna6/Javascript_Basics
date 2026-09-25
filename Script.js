d2264a879240ec1dc1fc6f0c85b8d497956d03d8
shivaramakrishna6 <shivaramakrishnaaakula@gmail.com>
2026-09-25 11:33:43 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,251 @@
+var name="Aakula Shiva Ramakrishna"
+console.log(name)
+// ------------------
+
+var age=23
+console.log(age)
+// -----------------------------
+
+var a
+    a=25
+    console.log(a)
+// --------------------------------
+
+var b=20
+
+b=40
+console.log(b)
+// -----------------------------------
+
+var c=30
+
+c=55
+console.log(c)
+// -------------------------------
+
+var name="Aakula Shiva Ramakrishna"
+var age=23
+var city="Hyderabad"
+// console.log("my name is",name)
+// console.log("my age is",age)
+// console.log("i am from",city)
+console.log(name,age,city)
+// -------------------------------------
+
+var college="St.Mary's Group Of Instutions Hyderabad"
+console.log(college)
+// ---------------------------------------------------------
+
+var subject="Physics"
+console.log(subject)
+// ------------------------------------------------------------
+
+var d=20
+
+d=40
+d=60
+d=80
+console.log(d)
+// ------------------------------------------
+
+var salary=150000
+
+salary=300000
+console.log(salary)
+// -------------------------------------------
+
+// -----------------------------USING let----------------------------------------------
+
+let _name="Shiva ram"
+console.log(_name)
+// ----------------------------------------------------
+
+let _age=23
+console.log(_age)
+// ----------------------------------------------
+
+let A=50
+console.log(A)
+// -----------------------------------------
+
+let B=100
+B=200
+console.log(B)
+// -------------------------------------
+
+let Name="shiva ramakrishna"
+let Qualification="Btech-AIML"
+let Location="Hyderabad"
+console.log(Name)
+console.log(Qualification)
+console.log(Location)
+// console.log(Name,Qualification,Location)
+// ---------------------------------------------
+
+let COURSE="JavaScript"
+
+// --------------------------------------------
+
+let marks=50
+marks=80
+marks=80
+console.log(marks)
+// ----------------------------------------------
+
+let companyName="STACKLY"
+console.log(companyName)
+// ---------------------------------------------
+
+let experience="2 year's"
+console.log(experience)
+// ------------------------------------------
+
+let mobileModel="IQOO Z9s"
+console.log(mobileModel)
+// --------------------------------------
+
+
+// -----------------------------------------------USING const ------------------------
+
+const NAME="Shiva Ramakrishna"
+console.log(NAME)
+// ---------------------------------------
+
+const DOB="09/01/2003"
+console.log(DOB)
+// ---------------------------------------
+
+const company="STACKLY"
+
+const country="INDIA"
+// ---------------------------------
+
+const _Name="Shiva ramakrishna"
+const _Age=23
+const City="Hyderabad"
+console.log(_Name,_Age,City)
+// ------------------------------------
+
+const courseName="JavaScript"
+// -----------------------------------
+
+const collegeName="St.Mary's Group Of Instutions Hyderabad"
+console.log(collegeName)
+// --------------------------------------------
+
+const favoriteColor="Green"
+console.log(favoriteColor)
+// -----------------------------------
+
+const employeeId="STK-26-6140"
+console.log(employeeId)
+// ------------------------------------
+
+const officeLocation="Hyderabad"
+console.log(officeLocation)
+// ----------------------------------------
+
+// -------------------------------Printing Statements---------------------------------
+
+let num=100
+console.log(num)
+// -----------------------------
+
+var name_ = "Shiva Ramakrishna"
+console.log(name_)
+// --------------------------------
+
+const AGE=23
+const qualification="Btech-AIML"
+console.log(AGE)
+console.log(qualification)
+// ------------------------------------
+
+console.log("Welcome to JavaScript")
+// ----------------------------------------
+
+console.log("one")
+console.log("two")
+console.log(3)
+console.log("four")
+console.log(5)
+// ---------------------------------
+
+let userName="Shiva_ramakrishna"
+console.log(userName)
+// ----------------------------------
+
+let userAge=23
+console.log(userAge)
+// ------------------------------------
+
+let _NAME="Shiva ramakrishna"
+let age_ =23
+let _city="Hyderabad"
+console.log(_NAME,age_,_city)
+// ----------------------------------------
+
+// -------------------------------------------POPUP Methods-------------------------
+
+alert("Welcome to JavaScript")
+// ----------------------------------
+
+let user_name="Shiva Ramakrishna"
+alert(user_name)
+// -------------------------------------
+
+let g=prompt("what is your name?")
+alert(g)
+// ----------------------------------------
+
+let h=prompt("Enter your age ?")
+console.log(h)
+// ------------------------------------------
+
+let i=prompt("what is your qualification ?")
+alert(i)
+// ------------------------------------------------
+
+let j=prompt("what is your city ?")
+console.log(j)
+// ------------------------------------------------
+
+confirm("Do you know JavaScript ?")
+confirm("Do you want to continue ?")
+// -------------------------------------------------------
+
+let k=prompt("what is your name ?")
+let l=prompt("what is your qualification ?")
+document.writeln(k)
+document.writeln(l)
+// -----------------------------------------------------------
+
+
+// ----------------------------------Document.writeln and console methods----------------------------
+
+let m='Hello Everyone'
+document.writeln("m")
+// --------------------------------
+
+console.log("shiva_ram")
+console.warn("Btech")
+console.error("Hyderabad")
+// -------------------------------------
+
+let n=prompt("what is your name ?")
+let o=prompt("what is your age ?")
+let p=prompt("what is your qualification ?")
+
+console.log(n)
+console.warn(o)
+console.error(p)
+
+document.writeln(n)
+document.writeln(o)
+document.writeln(p)
+
+
+
+// ------------------------------------------------------------------TASK 2------------------------------------------------------------------------------
+
+
